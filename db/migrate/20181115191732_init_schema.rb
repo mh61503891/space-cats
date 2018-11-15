5016461df1cc976883fd5c9fceb02a73e534d5ce
Masayuki Higashino <msyk@hgsn.info>
2018-11-16 05:48:48 +0900

    Add contents

---
+++ db/migrate/20181115191732_init_schema.rb
@@ -0,0 +1,23 @@
+class InitSchema < ActiveRecord::Migration[5.2]
+  def change
+
+    create_table :settings do |t|
+      t.text :data
+      t.timestamps null: false
+    end
+
+    create_table :contents do |t|
+      t.string :uri
+      t.string :open_graph_title
+      t.string :open_graph_type
+      t.string :open_graph_url
+      t.binary :open_graph_image_content, limit: 1024.megabytes
+      t.string :open_graph_image_url
+      t.string :open_graph_description
+      t.string :open_graph_site_name
+      t.timestamps null: false
+    end
+    add_index :contents, :uri, unique: true
+
+  end
+end
