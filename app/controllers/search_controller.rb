8a055ceea53651dc3bce4ebe5b82468d366066fc
Masayuki Higashino <mh.on.web@gmail.com>
2022-08-16 14:05:49 +0900

    no message

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,9 +5,11 @@ class SearchController < ApplicationController
   def index
     @query = params[:query]
     @contents = []
+    @notes = []
     @keywords = []
     if @query.present?
       @contents = Content.search(@query).order(updated_at: :desc)
+      @notes = Note.search(@query).order(updated_at: :desc)
       @keywords = Keyword.search(@query).order(updated_at: :desc)
     end
     render :index
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,19 @@
 class Note < ApplicationRecord
+
   has_many :note_contents, dependent: :destroy
   has_many :contents, through: :note_contents
+
+  validates :body, presence: true
+
+  include SearchCop
+
+  search_scope :search do
+    attributes(*%i(
+      id
+      body
+      created_at
+      updated_at
+    ))
+  end
+
 end
